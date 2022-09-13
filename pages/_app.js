c326c2e81b89b46bc02fe1ecf282cfc280491f10
Srijan Samridh <srijansinha3@gmail.com>
2022-09-13 11:02:02 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   pages/Components/Footer.js 	new file:   pages/Components/NavBar.js 	modified:   pages/_app.js 	new file:   pages/api/[pid].js 	new file:   pages/api/api.js 	new file:   pages/api/index.js 	modified:   pages/index.js 	new file:   pages/product/[slug].js 	new file:   pages/products.js 	new file:   postcss.config.js 	new file:   public/logo-landscape.png 	new file:   public/logo-mountain.png 	new file:   public/skis.jpg 	modified:   styles/globals.css 	new file:   tailwind.config.js 	new file:   yarn.lock

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
